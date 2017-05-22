682d3603e3bfbdd471487cceb9756932e8c4eaaa
Bohdan Benkevych <bohdan.benkevych@gmail.com>
2017-05-22 18:37:45 +0300

    category item page complete

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+$(".category-item__product-type").click(function () {
+  if (!$(this).children(".category-item__product-type__name").hasClass("category-item__product-type__name_active")) {
+    $(this).children(".category-item__product-type__name").addClass("category-item__product-type__name_active");
+    $(this).children(".category-item__product-type__name").children(".category-item__product-type__name__label").addClass("category-item__product-type__name__label_active");
+    $(this).siblings(".category-item__type-items").addClass("category-item__type-items_active");
+  } else {
+    $(this).children(".category-item__product-type__name").removeClass("category-item__product-type__name_active");
+    $(this).children(".category-item__product-type__name").children(".category-item__product-type__name__label").removeClass("category-item__product-type__name__label_active");
+    $(this).siblings(".category-item__type-items").removeClass("category-item__type-items_active");
+  }
+});
