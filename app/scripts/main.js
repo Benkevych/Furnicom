e0a95c0ce25f3651f747c4a4ce4ebc1c69daf62f
Bohdan Benkevych <bohdan.benkevych@gmail.com>
2017-05-23 12:19:28 +0300

    Cart page added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,3 +9,17 @@ $(".category-item__product-type").click(function () {
     $(this).siblings(".category-item__type-items").removeClass("category-item__type-items_active");
   }
 });
+
+$(".cart__items__item__change-btn").click(function () {
+  $(this).addClass("cart__items__item__change-btn_active");
+  $(this).siblings(".cart__items__item__count").removeAttr("disabled");
+  $(this).siblings(".cart__items__item__count").attr("enabled");
+  $(this).siblings(".cart__items__item__count").focus();
+});
+
+$(".cart__items__item__count").focusout(function () {
+  $(this).removeAttr("enabled");
+  $(this).attr("disabled", "true");
+  $(this).siblings(".cart__items__item__change-btn").removeClass("cart__items__item__change-btn_active");
+
+});
